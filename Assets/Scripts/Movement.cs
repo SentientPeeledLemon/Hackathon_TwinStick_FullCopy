74b422ee47ac3e2857a0c590d8ab20d859fe7186
dderas <tanav.somireddy@gmail.com>
2026-03-01 09:20:26 -0600

    test

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int dashTimer = 0;
+
+    public Vector3 mousePos;
+
+    public float speed = 10f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float dirX = Input.GetAxisRaw("Horizontal");
+        float dirZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 movementDir = new Vector3(dirX, 0, dirZ);
+        rb.linearVelocity += movementDir * speed * Time.deltaTime;
+
+        dashTimer++;
+        if (dashTimer >= 20)
+        {
+            Debug.Log("Dash Ready");
+        }
+        if (Keyboard.current.leftShiftKey.isPressed && dashTimer>=20)
+        {
+            rb.AddForce(new Vector3(dirX, 0, dirZ) * 50f, ForceMode.VelocityChange);
+            dashTimer=0;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Plane groundPlane = new Plane(Vector3.up, transform.position);
+
+        if (groundPlane.Raycast(ray, out float distance))
+        {
+            Vector3 cursorPos = ray.GetPoint(distance);
+
+            Vector3 direction = cursorPos - transform.position;
+            direction.y = 0f;
+
+            transform.RotateAround(transform.position, Vector3.up, Vector3.SignedAngle(transform.forward, direction, Vector3.up));
+        }
+    }
+}
